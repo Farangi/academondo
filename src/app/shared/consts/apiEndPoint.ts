01d0e4188516cabd2ac2baa3fc25f3a0b6364659
Simon Agerskov <snagerskov@gmail.com>
2017-03-24 10:24:59 +0100

    endpoints

--- src/app/shared/consts/apiEndPoint.ts
+++ src/app/shared/consts/apiEndPoint.ts
@@ -1,2 +1,3 @@
-export const apiEndPoint1 = 'http://localhost:8080';
-export const apiEndPoint = 'http://altruvation.mooo.com';
+export const apiEndPoint = 'http://localhost:8080';
+export const apiEndPoint1 = 'http://altruvation.mooo.com';
+export const apiEndPoint2 = 'http://endpoint.mooo.com';
